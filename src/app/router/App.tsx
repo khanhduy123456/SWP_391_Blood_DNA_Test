8bb547d1ad28cf847dfeccc8e33bd2382f1f8056
giabao25 <kan250403@gmail.com>
2025-05-31 21:00:41 +0700

    fix homepage

--- src/app/router/App.tsx
+++ src/app/router/App.tsx
@@ -1,12 +1,7 @@
-import Home from "@/page/home/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-export default function App(): React.ReactElement {
-  return (
-    <Router>
-      <Routes>
-        {/* Route mặc định cho trang chủ của khách hàng */}
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+import { AppRouter } from './Router';
+
+function App() {
+  return <AppRouter />;
 }
+
+export default App;
